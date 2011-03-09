245daa985e7a6d821b741c2682ea28d94540e5a6
waldemar.neto@gmail.com <waldemar.neto@gmail.com@491253a9-d88d-1b7e-99e1-504dfd555f6e>
2011-03-09 11:11:00 +0000

    z

--- trunk/be.edu.fundp.precise.bpel-ui.ui/src/be/edu/fundp/precise/bpel_ui/ui/ExtensionSampleUIConstants.java
+++ trunk/be.edu.fundp.precise.bpel-ui.ui/src/be/edu/fundp/precise/bpel_ui/ui/ExtensionSampleUIConstants.java
@@ -8,7 +8,7 @@ public class ExtensionSampleUIConstants {
 	public static final String DATAINPUT_ICON_16 = "dataInputIcon16/extensionactivity.gif";
 	public static final String DATAINPUT_ICON_20 = "dataInputIcon20/extensionactivity.png";
 	
-	public static final String DATAOUTPUT_ICON_16 = "dataoutput16/extensionactivity.gif";
-	public static final String DATAOUTPUT_ICON_20 = "dataoutput20/extensionactivity.png";
+	public static final String DATAOUTPUT_ICON_16 = "dataInputIcon16/extensionactivity.gif";
+	public static final String DATAOUTPUT_ICON_20 = "dataInputIcon20/extensionactivity.png";
 
 }
