d797449ad3105537122f8b8651bceb0c2dd0cbe3
waldemar.neto <waldemar.neto@gmail.com>
2011-03-18 09:09:16 +0000

    scope inicial

---
+++ scope.bpel_ui.ui/src/org/eclipse/bpel/extensionssample/ui/ExtensionSampleUIConstants.java
@@ -0,0 +1,9 @@
+package org.eclipse.bpel.extensionssample.ui;
+
+public class ExtensionSampleUIConstants {
+
+	// Bugzilla 324115
+	public static final String DEFAULT_ICON_16 = "obj16/extensionactivity.gif";
+	public static final String DEFAULT_ICON_20 = "obj20/extensionactivity.png";
+
+}
