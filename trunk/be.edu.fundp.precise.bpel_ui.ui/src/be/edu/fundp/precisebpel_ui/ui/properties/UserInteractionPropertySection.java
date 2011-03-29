dd0912633c1bf6ad530e97a5c1c84105989c83ab
waldemar.neto@gmail.com <waldemar.neto@gmail.com@491253a9-d88d-1b7e-99e1-504dfd555f6e>
2011-03-29 09:56:30 +0000

    Implemented DataSelection

--- trunk/be.edu.fundp.precise.bpel_ui.ui/src/be/edu/fundp/precisebpel_ui/ui/properties/UserInteractionPropertySection.java
+++ trunk/be.edu.fundp.precise.bpel_ui.ui/src/be/edu/fundp/precisebpel_ui/ui/properties/UserInteractionPropertySection.java
@@ -1,5 +1,8 @@
 package be.edu.fundp.precisebpel_ui.ui.properties;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.bpel.common.ui.details.IDetailsAreaConstants;
 import org.eclipse.bpel.common.ui.flatui.FlatFormAttachment;
 import org.eclipse.bpel.common.ui.flatui.FlatFormData;
@@ -18,14 +21,9 @@ import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.Hyperlink;
-import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
 
-import be.edu.fundp.precise.bpel_ui.model.DataInputUI;
 import be.edu.fundp.precise.bpel_ui.model.ModelFactory;
 import be.edu.fundp.precise.bpel_ui.model.UserInteraction;
 import be.edu.fundp.precise.bpel_ui.model.UserRole;
@@ -41,28 +39,31 @@ import be.edu.fundp.precisebpel_ui.ui.dialogs.NumberInputDialog;
 public class UserInteractionPropertySection extends BPELPropertySection {
 
 	protected Composite parentComposite;
-	//protected Label mainLabel
 	protected Section mainLabel;
 	protected Button variableBrowseButton;
-
+	protected List<Button> ourButton;
+	protected Composite sectionClient;
+	private Button deleteRoleButton;
+	private String myUserRole;
+	private List<Button> myButton;
 
 	@Override
 	protected MultiObjectAdapter[] createAdapters() {
 		MultiObjectAdapter[] l = super.createAdapters();
-		MultiObjectAdapter[] l2 = new MultiObjectAdapter[l.length+1];
+		MultiObjectAdapter[] l2 = new MultiObjectAdapter[l.length + 1];
 		for (int i = 0; i < l.length; i++) {
-			l2[i] = l[i]; 
+			l2[i] = l[i];
 		}
 		l2[l.length] = new BatchedMultiObjectAdapter() {
-				
-				@Override
-				public void notify (Notification n) {
-				}
-				
-				@Override
-				public void finish() {
-					updateVariableWidgets();
-				}
+
+			@Override
+			public void notify(Notification n) {
+			}
+
+			@Override
+			public void finish() {
+				updateVariableWidgets();
+			}
 		};
 		return l2;
 	}
@@ -70,96 +71,150 @@ public class UserInteractionPropertySection extends BPELPropertySection {
 	@Override
 	protected void createClient(Composite parent) {
 		FlatFormData data;
+		
+		myButton = new LinkedList<Button>();
 
-		final Composite composite = parentComposite = createFlatFormComposite( createFlatFormComposite(parent) );
+		final Composite composite = parentComposite = createFlatFormComposite(createFlatFormComposite(parent));
 		data = new FlatFormData();
 		data.left = new FlatFormAttachment(0, 0);
 		data.right = new FlatFormAttachment(100, 0);
-		data.top = new FlatFormAttachment(composite, IDetailsAreaConstants.VSPACE);
+		data.top = new FlatFormAttachment(0, 0);
 		composite.setLayoutData(data);
 
-		Label variableLabel = fWidgetFactory.createLabel(composite, "Roles:"); 
+		// Label variableLabel = fWidgetFactory.createLabel(composite,
+		// "Roles:");
 		//mainLabel = fWidgetFactory.createScrolledForm(parent)Label(composite, "", SWT.NONE); //$NON-NLS-1$
-		
 		mainLabel = fWidgetFactory.createSection(composite, SWT.NONE); //$NON-NLS-1$
-		
+
 		mainLabel.setText("Section title");
-		mainLabel.setDescription("This is the description that goes "+
-				      "below the title");
-				 Composite sectionClient = fWidgetFactory.createComposite(mainLabel);
-				 sectionClient.setLayout(new GridLayout());
-				 Button button1 = fWidgetFactory.createButton(sectionClient, "Radio 1", SWT.RADIO);
-				 Button button2 = fWidgetFactory.createButton(sectionClient, "Radio 2", SWT.RADIO);
-				 mainLabel.setClient(sectionClient);
-		
-		variableBrowseButton = fWidgetFactory.createButton(composite, "Create Role", SWT.PUSH); 
+		mainLabel.setDescription("This is the description that goes "
+				+ "below the title");
+		sectionClient = fWidgetFactory.createComposite(mainLabel);
+		sectionClient.setLayout(new GridLayout());
+		// Button button2 = fWidgetFactory.createButton(sectionClient,
+		// "Radio 2", SWT.RADIO);
+		mainLabel.setClient(sectionClient);
+
+		variableBrowseButton = fWidgetFactory.createButton(composite,
+				"Create Role", SWT.PUSH);
+
+		deleteRoleButton = fWidgetFactory.createButton(composite,
+				"Delete Role", SWT.PUSH);
 
 		data = new FlatFormData();
-		data.left = new FlatFormAttachment(0, BPELUtil.calculateLabelWidth(variableLabel, STANDARD_LABEL_WIDTH_SM));
-		data.right = new FlatFormAttachment(variableBrowseButton, -IDetailsAreaConstants.HSPACE);
+		data.left = new FlatFormAttachment(0, 0);
+		data.right = new FlatFormAttachment(10, 0);
 		data.top = new FlatFormAttachment(0, 0);
-		data.height = FigureUtilities.getTextExtents(variableBrowseButton.getText(), variableBrowseButton.getFont()).height + 4;
+		data.height = FigureUtilities.getTextExtents(
+				variableBrowseButton.getText(), variableBrowseButton.getFont()).height + 4;
 		mainLabel.setLayoutData(data);
 
 		data = new FlatFormData();
 		data.top = new FlatFormAttachment(mainLabel, 0, SWT.TOP);
-		data.bottom = new FlatFormAttachment(mainLabel, 2, SWT.BOTTOM);
-		data.left = new FlatFormAttachment(50, -BPELUtil.calculateButtonWidth(variableBrowseButton,
-				SHORT_BUTTON_WIDTH)-IDetailsAreaConstants.CENTER_SPACE);
-		data.right = new FlatFormAttachment(50, -IDetailsAreaConstants.CENTER_SPACE);
+		// data.bottom = new FlatFormAttachment(mainLabel, 2, SWT.BOTTOM);
+		data.left = new FlatFormAttachment(50, -BPELUtil.calculateButtonWidth(
+				variableBrowseButton, SHORT_BUTTON_WIDTH)
+				- IDetailsAreaConstants.CENTER_SPACE);
+		data.right = new FlatFormAttachment(50,
+				-IDetailsAreaConstants.CENTER_SPACE);
 		variableBrowseButton.setLayoutData(data);
-		
-		data = new FlatFormData();
-		data.left = new FlatFormAttachment(0, 0);
-		data.right = new FlatFormAttachment(mainLabel, -IDetailsAreaConstants.HSPACE);
-		data.top = new FlatFormAttachment(mainLabel, 0, SWT.CENTER);
-		variableLabel.setLayoutData(data);
-		
+
 		variableBrowseButton.addSelectionListener(new SelectionListener() {
 			public void widgetSelected(SelectionEvent e) {
 				Shell shell = composite.getShell();
-				//I need to create a command
-				//NumberInputDialog dialog = new NumberInputDialog(shell);
-			   
+				// I need to create a command
+				// NumberInputDialog dialog = new NumberInputDialog(shell);
+
 				EObject model = getUserInteraction();
-				NumberInputDialog dialog = new NumberInputDialog(shell, model, 
-						ModelHelper.getVariableType(model, ModelHelper.OUTGOING));
-				 String id = dialog.open();
-				 UserRole l = ModelFactory.eINSTANCE.createUserRole();
-				 l.setId(id);
-				 if (model instanceof UserInteraction){
-					 UserInteraction inter = (UserInteraction)model;
-					 inter.getRole().add(l);
-				 }
-//				dialog.setTitle(Messages.FaultThrowNameSection_Select_Fault_Variable_2); 
-//				if (dialog.open() == Window.OK) {
-//					Variable variable = dialog.getVariable();
-//					Command command = new SetVariableCommand(model, variable);
-//					getCommandFramework().execute(wrapInShowContextCommand(command));
-//				}
+				NumberInputDialog dialog = new NumberInputDialog(shell, model,
+						ModelHelper
+								.getVariableType(model, ModelHelper.OUTGOING));
+				String id = dialog.open();
+				if (id != null) {
+					UserRole l = ModelFactory.eINSTANCE.createUserRole();
+					l.setId(id);
+					if (model instanceof UserInteraction) {
+						UserInteraction inter = (UserInteraction) model;
+						inter.getRole().add(l);
+						basicSetInput(model);
+					}
+					updateVariableWidgets();
+				}
 			}
-			public void widgetDefaultSelected(SelectionEvent e) { widgetSelected(e); }
+
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
+			}
+		});
+
+		data = new FlatFormData();
+		data.top = new FlatFormAttachment(mainLabel, 25, SWT.TOP);
+		// data.bottom = new FlatFormAttachment(mainLabel, 2, SWT.BOTTOM);
+		data.left = new FlatFormAttachment(50, -BPELUtil.calculateButtonWidth(
+				deleteRoleButton, SHORT_BUTTON_WIDTH)
+				- IDetailsAreaConstants.CENTER_SPACE);
+		data.right = new FlatFormAttachment(50,
+				-IDetailsAreaConstants.CENTER_SPACE);
+		deleteRoleButton.setLayoutData(data);
+
+		deleteRoleButton.addSelectionListener(new SelectionListener() {
+			public void widgetSelected(SelectionEvent e) {
+				EObject model = getUserInteraction();
+				if (myUserRole != null) {
+					UserInteraction inter = (UserInteraction) model;
+					EList<UserRole> l = inter.getRole();
+					for (UserRole userRole : l) {
+						if (userRole.getId().equals(myUserRole)) {
+							l.remove(userRole);
+							break;
+						}
+					}
+					updateVariableWidgets();
+				}
+			}
+
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
+			}
 		});
 	}
-	
+
 	public void updateVariableWidgets() {
-		String label = "";
+		String label = "Roles";
 		EObject model = getUserInteraction();
-		if (model instanceof UserInteraction){
-			 UserInteraction inter = (UserInteraction)model;
-			 EList<UserRole> l = inter.getRole();
-			 for (UserRole userRole : l) {
-				label += " - "+userRole.getId();
-			 }
-			 mainLabel.setText(label);
-		 } else {
-			 mainLabel.setText("teste");
-		 }
+		mainLabel.setText(label);
+		if (model instanceof UserInteraction) {
+			UserInteraction inter = (UserInteraction) model;
+			EList<UserRole> l = inter.getRole();
+			raiseButton();
+			for (final UserRole userRole : l) {
+				System.out.println("role "+userRole);
+				Button button2 = fWidgetFactory.createButton(sectionClient,
+						userRole.getId(), SWT.RADIO);
+				button2.addSelectionListener(new SelectionListener() {
+					public void widgetSelected(SelectionEvent e) {
+						myUserRole = userRole.getId();
+					}
+
+					public void widgetDefaultSelected(SelectionEvent e) {
+						widgetSelected(e);
+					}
+				});
+				myButton.add(button2);
+			}
+		}
 	}
-	
+
+	private void raiseButton() {
+		System.out.println("disposed");
+		for (Button theButton : myButton) {
+			theButton.dispose();
+		}
+	}
+
 	@Override
 	protected void basicSetInput(EObject newInput) {
-		if ( newInput instanceof UserInteraction) {
+		if (newInput instanceof UserInteraction) {
 			super.basicSetInput(newInput);
 			updateVariableWidgets();
 		}
