ab55c8cb10373851717c8068c24d5224a5bd548b
waldemar.neto@gmail.com <waldemar.neto@gmail.com@491253a9-d88d-1b7e-99e1-504dfd555f6e>
2011-03-30 12:29:44 +0000

    unused projects

--- test.be.edu.fundp.precisebpel_ui.ui/trunk/test.be.edu.fundp.precisebpel_ui.ui/src/be/edu/fundp/precisebpel_ui/ui/BPEL_UIConstants.java
+++
@@ -1,8 +0,0 @@
-package be.edu.fundp.precisebpel_ui.ui;
-
-public class BPEL_UIConstants {
-
-	public static final String DEFAULT_ICON_16 = "obj16/extensionactivity.gif";
-	public static final String DEFAULT_ICON_20 = "obj20/extensionactivity.png";
-
-}
