5a551750f459636cff892b5007a6e5d900ad8426
waldemar.neto@gmail.com <waldemar.neto@gmail.com@491253a9-d88d-1b7e-99e1-504dfd555f6e>
2012-04-03 09:24:36 +0000

    bug fixing

--- travel_process/UiManager/src/be/ac/fundp/uimanager/UiManagerLogic.java
+++ travel_process/UiManager/src/be/ac/fundp/uimanager/UiManagerLogic.java
@@ -26,54 +26,56 @@ import be.ac.fundp.uimanager.dispatcher.Dispatcher;
 import be.ac.fundp.uimanager.dispatcher.restDispatcher.RestDispacher;
 import be.ac.fundp.uimanager.model.ProvidedData;
 
-
 public class UiManagerLogic {
-	
+
 	static {
-	    Configuration configuration = new Configuration();
-	    configuration.configure();
-	    ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
-	    configureSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
+				.applySettings(configuration.getProperties())
+				.buildServiceRegistry();
+		configureSessionFactory = configuration
+				.buildSessionFactory(serviceRegistry);
 	}
-	
+
 	static UiManagerLogic self;
-	
+
 	public static SessionFactory configureSessionFactory;
-	
+
 	private int counter = 1;
-	
+
 	private String HEAD_PROCESS = "Trip";
-	
-	protected UiManagerLogic(){
+
+	protected UiManagerLogic() {
 		Role role = new Role();
 		role.setRoleId("manager");
-		
+
 		User user = new User();
 		user.setUserId("philippe");
 		user.setPassword("philippe");
 		user.setAvailable(true);
 		user.getRole().add(role);
-		
+
 		Context context = new Context();
-		//http://localhost:8070/UsiXML-WebClient/restlet/test
+		// http://localhost:8070/UsiXML-WebClient/restlet/test
 		context.setIpAddress("http://localhost:8070/UsiXML-WebClient/restlet/uibpel/");
 		context.setProtocolType(ProtocolType.Rest);
 		user.setContext(context);
-		
+
 		Role role2 = new Role();
 		role2.setRoleId("administrator");
-		
+
 		User user2 = new User();
 		user2.setUserId("mohamed");
 		user2.setPassword("mohamed");
 		user2.setAvailable(true);
 		user2.getRole().add(role2);
-		
+
 		Context context2 = new Context();
 		context2.setIpAddress("http://localhost:8070/UsiXML-WebClient/restlet/uibpel/");
 		context2.setProtocolType(ProtocolType.Rest);
 		user2.setContext(context2);
-		
+
 		Session session = configureSessionFactory.openSession();
 		session.beginTransaction();
 		session.saveOrUpdate(role);
@@ -86,8 +88,8 @@ public class UiManagerLogic {
 		session.close();
 
 	}
-	
-	public static UiManagerLogic getInstance(){
+
+	public static UiManagerLogic getInstance() {
 		if (self == null)
 			self = new UiManagerLogic();
 		return self;
@@ -116,31 +118,32 @@ public class UiManagerLogic {
 		Role roleObj = (Role) session.get(Role.class, role);
 		idClass.setRole(roleObj);
 		idClass.setProcess(process);
-		//ProcessBind processBind = (ProcessBind) session.get(ProcessBind.class, idClass);
-		ProcessBind processBind = (ProcessBind)session.createCriteria(ProcessBind.class)
+		ProcessBind processBind = (ProcessBind) session
+				.createCriteria(ProcessBind.class)
 				.add(Restrictions.eq("id", idClass)).uniqueResult();
 		if (processBind == null) {
 			processBind = new ProcessBind();
 			processBind.setId(idClass);
 			List<User> userList = null;
-			do {
+			OUTERMOST: do {
 				session.getTransaction().commit();
 				session.beginTransaction();
 				userList = session.createCriteria(User.class)
 						.add(Restrictions.eq("isAvailable", true)).list();
 
-				if (userList != null){
+				if (userList != null) {
 					for (User anyUser : userList) {
-						System.out.println("user="+anyUser.getUserId());
+						System.out.println("user=" + anyUser.getUserId());
 						for (Role roleAnyUser : anyUser.getRole()) {
-							System.out.println("roleAnyUser="+roleAnyUser.getRoleId());
-							System.out.println("ExpectedRole="+role);
-							if (roleAnyUser.getRoleId().equalsIgnoreCase(role)){
+							System.out.println("roleAnyUser="
+									+ roleAnyUser.getRoleId());
+							System.out.println("ExpectedRole=" + role);
+							if (roleAnyUser.getRoleId().equalsIgnoreCase(role)) {
 								System.out.println("yeaaaaa");
 								user = anyUser;
 								user.setAvailable(false);
 								session.saveOrUpdate(user);
-								break;
+								break OUTERMOST;
 							}
 						}
 					}
@@ -153,11 +156,11 @@ public class UiManagerLogic {
 					}
 				}
 			} while (user == null);
-		    
-		    processBind.setUser(user);
-		    process.getProcessBind().add(processBind);
-		    session.saveOrUpdate(processBind);
-		    session.saveOrUpdate(process);
+
+			processBind.setUser(user);
+			process.getProcessBind().add(processBind);
+			session.saveOrUpdate(processBind);
+			session.saveOrUpdate(process);
 		} else {
 			user = processBind.getUser();
 		}
@@ -192,9 +195,9 @@ public class UiManagerLogic {
 		session.getTransaction().commit();
 		session.close();
 	}
-	
+
 	private Dispatcher getDispatcher(Context context) {
-		System.out.println("Context="+context);
+		System.out.println("Context=" + context);
 		if (context.getProtocolType().equals(ProtocolType.Rest))
 			return new RestDispacher(context.getIpAddress());
 		return null;
@@ -202,24 +205,25 @@ public class UiManagerLogic {
 
 	public String generateId() {
 		String uuid = UUID.randomUUID().toString();
-		uuid = HEAD_PROCESS+counter+uuid;
+		uuid = HEAD_PROCESS + counter + uuid;
 		counter++;
 		return uuid;
 	}
 
-	public void subscribe(String login, String password, String role, String ipAddress) {
+	public void subscribe(String login, String password, String role,
+			String ipAddress) {
 		User newUser = new User();
 		Context newContext = new Context();
 		createRoleIfNecessary(role);
-		
+
 		newContext.setIpAddress(ipAddress);
 		newContext.setProtocolType(ProtocolType.Rest);
-		
+
 		newUser.setPassword(password);
 		newUser.setUserId(login);
 		newUser.setContext(newContext);
 		newUser.setAvailable(true);
-		
+
 		Session session = configureSessionFactory.openSession();
 		session.beginTransaction();
 		Role userRole = (Role) session.get(Role.class, role);
@@ -234,7 +238,7 @@ public class UiManagerLogic {
 		Session session = configureSessionFactory.openSession();
 		session.beginTransaction();
 		User user = (User) session.get(User.class, login);
-		if (user != null && user.getPassword().equals(password)){
+		if (user != null && user.getPassword().equals(password)) {
 			return user.getRole().iterator().next().getRoleId();
 		}
 		session.getTransaction().commit();
@@ -255,10 +259,10 @@ public class UiManagerLogic {
 
 	public List<ProvidedData> requireInputInteracion(String role,
 			String processId, String userInteracId) {
-		
+
 		createRoleIfNecessary(role);
 		createProcessIfNecessary(processId);
-		
+
 		Session session = configureSessionFactory.openSession();
 		session.beginTransaction();
 
@@ -268,18 +272,20 @@ public class UiManagerLogic {
 
 		String userId = getUser(role, processId);
 		User user = (User) session.get(User.class, userId);
-		
+
 		Process process = (Process) session.get(Process.class, processId);
 		process.getInteractions().add(interaction);
 		session.saveOrUpdate(interaction);
 		session.saveOrUpdate(process);
 		session.getTransaction().commit();
-		
-		List<ProvidedData> reponse = getDispatcher(user.getContext()).requireInputInteracion(processId, userInteracId, role);
-		
+
+		List<ProvidedData> reponse = getDispatcher(user.getContext())
+				.requireInputInteracion(processId, userInteracId, role);
+
 		session.beginTransaction();
-		interaction = (Interaction) session.get(Interaction.class, interaction.getInteractionRealId());
-		
+		interaction = (Interaction) session.get(Interaction.class,
+				interaction.getInteractionRealId());
+
 		for (ProvidedData providedData : reponse) {
 			DataItem item = new DataItem();
 			item.setItemId(providedData.getId());
@@ -289,9 +295,9 @@ public class UiManagerLogic {
 			interaction.getProvidedData().add(item);
 			session.saveOrUpdate(item);
 		}
-		
+
 		interaction.setFinished(true);
-		
+
 		session.saveOrUpdate(interaction);
 		session.getTransaction().commit();
 		session.close();
@@ -304,10 +310,10 @@ public class UiManagerLogic {
 		Interaction interaction = new Interaction();
 		interaction.setInteractionId(userInteracId);
 		interaction.setFinished(false);
-		
+
 		Session session = configureSessionFactory.openSession();
 		session.beginTransaction();
-		
+
 		for (ProvidedData providedData : uiDataType2ProvidedData) {
 			DataItem item = new DataItem();
 			item.setItemId(providedData.getId());
@@ -317,25 +323,27 @@ public class UiManagerLogic {
 			interaction.getAvailableData().add(item);
 			session.saveOrUpdate(item);
 		}
-		
+
 		createRoleIfNecessary(role);
 		createProcessIfNecessary(processId);
-		
+
 		String userId = getUser(role, processId);
-		
-		
+
 		User user = (User) session.get(User.class, userId);
 		Process process = (Process) session.get(Process.class, processId);
 		session.saveOrUpdate(interaction);
 		process.getInteractions().add(interaction);
 		session.saveOrUpdate(process);
 		session.getTransaction().commit();
-		
-		List<ProvidedData> reponse = getDispatcher(user.getContext()).requireSelectionInteracion(processId, userInteracId, uiDataType2ProvidedData, role);
-		
+
+		List<ProvidedData> reponse = getDispatcher(user.getContext())
+				.requireSelectionInteracion(processId, userInteracId,
+						uiDataType2ProvidedData, role);
+
 		session.beginTransaction();
-		interaction = (Interaction) session.get(Interaction.class, interaction.getInteractionRealId());
-		
+		interaction = (Interaction) session.get(Interaction.class,
+				interaction.getInteractionRealId());
+
 		for (ProvidedData providedData : reponse) {
 			DataItem item = new DataItem();
 			item.setItemId(providedData.getId());
@@ -345,26 +353,26 @@ public class UiManagerLogic {
 			interaction.getProvidedData().add(item);
 			session.saveOrUpdate(item);
 		}
-		
+
 		interaction.setFinished(true);
 		session.saveOrUpdate(interaction);
-		
+
 		session.getTransaction().commit();
 		session.close();
-		
+
 		return reponse;
 	}
 
 	public void requireOutputInteracion(String processId, String userInteracId,
 			List<ProvidedData> uiDataType2ProvidedData, String role) {
-		
+
 		Interaction interaction = new Interaction();
 		interaction.setInteractionId(userInteracId);
 		interaction.setFinished(false);
-		
+
 		Session session = configureSessionFactory.openSession();
 		session.beginTransaction();
-		
+
 		for (ProvidedData providedData : uiDataType2ProvidedData) {
 			DataItem item = new DataItem();
 			item.setItemId(providedData.getId());
@@ -374,13 +382,13 @@ public class UiManagerLogic {
 			interaction.getAvailableData().add(item);
 			session.saveOrUpdate(item);
 		}
-		
+
 		createRoleIfNecessary(role);
-		
+
 		createProcessIfNecessary(processId);
-		
+
 		String userId = getUser(role, processId);
-		
+
 		User user = (User) session.get(User.class, userId);
 		Process process = (Process) session.get(Process.class, processId);
 		process.getInteractions().add(interaction);
@@ -388,14 +396,15 @@ public class UiManagerLogic {
 		session.saveOrUpdate(process);
 		session.getTransaction().commit();
 		session.close();
-		
-		getDispatcher(user.getContext()).requireOutputInteracion(processId, userInteracId, uiDataType2ProvidedData, role);
-		
-		if (userInteracId.equals("30") || userInteracId.equals("31")){
+
+		getDispatcher(user.getContext()).requireOutputInteracion(processId,
+				userInteracId, uiDataType2ProvidedData, role);
+
+		if (userInteracId.equals("28") || userInteracId.equals("31")) {
 			releaseRole(role, processId);
 		}
-		
-		if (userInteracId.equals("29") || userInteracId.equals("32")){
+
+		if (userInteracId.equals("29") || userInteracId.equals("32")) {
 			finishProcess(role, processId);
 			releaseRole(role, processId);
 		}
@@ -410,6 +419,6 @@ public class UiManagerLogic {
 		session.saveOrUpdate(user);
 		session.getTransaction().commit();
 		session.close();
-		
+
 	}
 }
